0d6899b17197702506a733ea73b220189b253cc4
Lukas <46488078+lukas-stop@users.noreply.github.com>
2023-11-29 10:53:31 -0500

    update og data to be organized + labeled (progress save)

--- js/emotions.js
+++ js/emotions.js
@@ -1,4 +1,4 @@
-const emotionData = {
+const geoffreyEmotionWheel = {
     "emotions": [
         {
             "Happy": [{
@@ -57,149 +57,222 @@ const emotionData = {
             }
             ]
         }, {
-
+            "Surprised": [
+                {
+                    "Startled": [
+                        "Shocked",
+                        "Dismayed"
+                    ],
+                },
+                {
+                    "Confused": [
+                        "Disillusioned",
+                        "Perplexed"
+                    ],
+                },
+                {
+                    "Amazed": [
+                        "Astonished",
+                        "Awe"
+                    ],
+                },
+                {
+                    "Excited": [
+                        "Eager",
+                        "Energetic"
+                    ],
+                }
+            ]
+        },
+        {
+            "Bad": [
+                {
+                    "Bored": [
+                        "Indifferent",
+                        "Apathetic"
+                    ],
+                },
+                {
+                    "Busy": [
+                        "Pressured",
+                        "Rushed"
+                    ],
+                },
+                {
+                    "Stressed": [
+                        "Overwhelmed",
+                        "Out of Control"
+                    ],
+                },
+                {
+                    "Tired": [
+                        "Sleepy",
+                        "Unfocused"
+                    ]
+                }
+            ]
+        },
+        {
+            "Fearful": [
+                {
+                    "Scared": [
+                        "Helpless",
+                        "Frightened"
+                    ],
+                },
+                {
+                    "Anxious": [
+                        "Overwhelmed",
+                        "Worried"
+                    ],
+                },
+                {
+                    "Insecure": [
+                        "Inadequate",
+                        "Inferior"
+                    ],
+                },
+                {
+                    "Weak": [
+                        "Worthless",
+                        "Insignificant"
+                    ],
+                },
+                {
+                    "Rejected": [
+                        "Excluded",
+                        "Persecuted"
+                    ],
+                },
+                {
+                    "Threatened": [
+                        "Nervous",
+                        "Exposed"
+                    ]
+                }
+            ]
+        },
+        {
+            "Angry": [
+                {
+                    "Let Down": [
+                        "Betrayed",
+                        "Resentful"
+                    ]
+                },
+                {
+                    "Humiliated": [
+                        "Disrespected",
+                        "Ridiculed"
+                    ]
+                },
+                {
+                    "Bitter": [
+                        "Indignant",
+                        "Violated"
+                    ]
+                },
+                {
+                    "Mad": [
+                        "Furious",
+                        "Jealous"
+                    ]
+                },
+                {
+                    "Aggressive": [
+                        "Provoked",
+                        "Hostile"
+                    ]
+                },
+                {
+                    "Frustrated": [
+                        "Infuriated",
+                        "Annoyed"
+                    ]
+                },
+                {
+                    "Distant": [
+                        "Withdrawn",
+                        "Numb"
+                    ]
+                },
+                {
+                    "Critical": [
+                        "Skeptical",
+                        "Dismissive"
+                    ]
+                }
+            ]
+        },
+        {
+            "Disgusted": [
+                {
+                    "Disapproving": [
+                        "Judgmental",
+                        "Embarrassed"
+                    ]
+                },
+                {
+                    "Disappointed": [
+                        "Appalled",
+                        "Revolted"
+                    ]
+                },
+                {
+                    "Awful": [
+                        "Nauseated",
+                        "Detestable"
+                    ]
+                },
+                {
+                    "Repelled": [
+                        "Horrified",
+                        "Hesitant"
+                    ]
+                }
+            ]
+        },
+        {
+            "Sad": [
+                {
+                    "Lonely": [
+                        "Isolated",
+                        "Abandoned"
+                    ]
+                },
+                {
+                    "Vulnerable": [
+                        "Victimized",
+                        "Fragile"
+                    ]
+                },
+                {
+                    "Despair": [
+                        "Grief",
+                        "Powerless"
+                    ]
+                },
+                {
+                    "Guilty": [
+                        "Ashamed",
+                        "Remorseful"
+                    ]
+                },
+                {
+                    "Depressed": [
+                        "Empty",
+                        "Inferior"
+                    ]
+                },
+                {
+                    "Hurt": [
+                        "Embarrassed",
+                        "Disappointed"
+                    ]
+                }
+            ]
         }
     ]
 }
 
-// },
-// "Surprised": {
-//     "Startled": [
-//         "Shocked",
-//         "Dismayed"
-//     ],
-//     "Confused": [
-//         "Disillusioned",
-//         "Perplexed"
-//     ],
-//     "Amazed": [
-//         "Astonished",
-//         "Awe"
-//     ],
-//     "Excited": [
-//         "Eager",
-//         "Energetic"
-//     ]
-// },
-// "Bad": {
-//     "Bored": [
-//         "Indifferent",
-//         "Apathetic"
-//     ],
-//     "Busy": [
-//         "Presured",
-//         "Rushed"
-//     ],
-//     "Stressed": [
-//         "Overwhelmed",
-//         "Out of Control"
-//     ],
-//     "Tired": [
-//         "Sleepy",
-//         "Unfocused"
-//     ]
-// },
-// "Fearful": {
-//     "Scared": [
-//         "Helpless",
-//         "Frightened"
-//     ],
-//     "Anxious": [
-//         "Overwhelmed",
-//         "Worried"
-//     ],
-//     "Insecure": [
-//         "Inadequate",
-//         "Inferior"
-//     ],
-//     "Weak": [
-//         "Worthless",
-//         "Insignificant"
-//     ],
-//     "Rejected": [
-//         "Exclused",
-//         "Persecuted"
-//     ],
-//     "Threatened": [
-//         "Nervous",
-//         "Exposed"
-//     ]
-// },
-// "Angry": {
-//     "Let Down": [
-//         "Betrayed",
-//         "Resentful"
-//     ],
-//     "Humiliated": [
-//         "Disrespected",
-//         "Ridiculed"
-//     ],
-//     "Bitter": [
-//         "Indignant",
-//         "Violated"
-//     ],
-//     "Mad": [
-//         "Furious",
-//         "Jealous"
-//     ],
-//     "Aggressive": [
-//         "Provoked",
-//         "Hostile"
-//     ],
-//     "Frustrated": [
-//         "Infuriated",
-//         "Annoyed"
-//     ],
-//     "Distant": [
-//         "Withdrawn",
-//         "Numb"
-//     ],
-//     "Critical": [
-//         "Sceptical",
-//         "Dismissive"
-//     ]
-// },
-// "Disgusted": {
-//     "Disapproving": [
-//         "Judgmental",
-//         "Embarrassed"
-//     ],
-//     "Disappointed": [
-//         "Appalled",
-//         "Revolted"
-//     ],
-//     "Awful": [
-//         "Nauseated",
-//         "Detestable"
-//     ],
-//     "Repelled": [
-//         "Horrified",
-//         "Hesitant"
-//     ]
-// },
-// "Sad": {
-//     "Lonely": [
-//         "Isolated",
-//         "Abandoned"
-//     ],
-//     "Vulnerable": [
-//         "Victimised",
-//         "Fragile"
-//     ],
-//     "Despair": [
-//         "Grief",
-//         "Powerless"
-//     ],
-//     "Guilty": [
-//         "Ashamed",
-//         "Remorseful"
-//     ],
-//     "Depressed": [
-//         "Empty",
-//         "Inferior"
-//     ],
-//     "Hurt": [
-//         "Embarassed",
-//         "Disappointed"
-//     ]
-// }
+
